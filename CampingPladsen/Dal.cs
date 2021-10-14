700e57a4321d4c26c6f28174be82760d713cb5e0
Mikkel-P <83957934+Mikkel-P@users.noreply.github.com>
2021-10-14 14:07:48 +0200

    Comments

--- CampingPladsen/Dal.cs
+++ CampingPladsen/Dal.cs
@@ -13,10 +13,13 @@ namespace CampingProjekt
         /// <returns></returns>
         public SqlConnection NewConDal()
         {
+            // Creates a new SqlConnection object
             SqlConnection newCon = new SqlConnection();
 
+            // Creates a SqlConnectionStringBuilder object which gets its connectionstring from the GetConString method.
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(GetConString());
 
+            // Gets the connectionstring from the SqlConnectionStringBuilder object, and copies it to the SqlConnection object
             newCon.ConnectionString = builder.ConnectionString;
 
             return newCon;
@@ -28,6 +31,7 @@ namespace CampingProjekt
         /// <returns></returns>
         private static string GetConString()
         {
+            // Returns a string with the information needed to make a connectionstring
             return "Data Source=172.16.59.46;Initial Catalog=CampingDB;Persist Security Info=True;User ID=sa;Password=Password1234!";
         }
     }
